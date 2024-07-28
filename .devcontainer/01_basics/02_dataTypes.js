dfae9a37ecf41e5135e593335e47468935794393
SohailBhatti-777 <146609984+SohailBhatti-777@users.noreply.github.com>
2024-07-28 12:47:08 +0000

    add 2-files done

---
+++ .devcontainer/01_basics/02_dataTypes.js
@@ -0,0 +1,34 @@
+"use strict";     // treat all JS code as newer version
+
+//  alert(3 + 3)  // we are using nodejs, not browser
+
+console.log(3 + 3);
+console.log("sohail");
+console.log(6 
+  + 
+  3);                     //code readabliliy should be high
+
+// MDN Reference
+// https://tc39.es/ecma262/
+
+let name = "sohail"       // string datatype
+let age = 18              // number 
+let isLoggedIn = false    // boolean
+
+// PRIMITIVE DATA TYPES
+// number  => 2 to power 53
+// bigint
+// string  => ""
+// boolean => true/ false
+// null    => standalone value (representation of empty value)
+// undefined => variable is declared but value is not assigned
+// symbol  => unique
+
+// OBJECT  DATA TYPES
+
+
+console.log(typeof "sohail");     // string
+console.log(typeof age);          // number
+console.log(typeof null);         // object
+console.log(typeof undefined);    // undefined
+
